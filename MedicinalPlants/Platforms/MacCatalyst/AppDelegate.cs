fc59a49aa99c16f93297918aaaddff4219b40cc8
Diego Pardo <dp.lenin@msn.com>
2022-12-19 19:09:09 -0500

    Add project files.

---
+++ MedicinalPlants/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MedicinalPlants;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
