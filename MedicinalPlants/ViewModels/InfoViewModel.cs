56914016ec9c6adbdce5fee1842bad6bff11ec80
Diego Pardo <dp.lenin@msn.com>
2022-12-27 20:32:33 -0500

    Add infopage

---
+++ MedicinalPlants/ViewModels/InfoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Input;
+
+namespace MedicinalPlants.ViewModels
+{
+    public class InfoViewModel : BaseViewModel
+    {
+        public InfoViewModel()
+        {
+            Title = "Info";
+        }
+
+        public ICommand TapLinkCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
+    }
+}
---
+++ MedicinalPlants/Views/InfoPage.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Input;
+
+namespace MedicinalPlants.Views;
+
+public partial class InfoPage : ContentPage
+{
+    public InfoPage()
+	{
+		InitializeComponent();
+    }
+}
